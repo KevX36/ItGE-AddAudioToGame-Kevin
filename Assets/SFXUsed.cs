d6e13859a943eae1779d813134aa77b091ed6f44
KevX36 <kevincard2007@gmail.com>
2025-11-05 11:09:11 -0400

    Got Beam SFX

---
+++ Assets/SFXUsed.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class SFXUsed : MonoBehaviour
+{
+    // Sound Effect by <a href="https://pixabay.com/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47339">freesound_community</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47339">Pixabay</a>
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+}
