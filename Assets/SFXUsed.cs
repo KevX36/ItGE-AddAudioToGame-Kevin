75d0e131464dc0e4ad87571b50479f15ec29def2
KevX36 <kevincard2007@gmail.com>
2025-11-05 11:26:10 -0400

    added 2 more SFX

--- Assets/SFXUsed.cs
+++ Assets/SFXUsed.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class SFXUsed : MonoBehaviour
 {
     // Sound Effect by <a href="https://pixabay.com/users/freesound_community-46691455/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47339">freesound_community</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=47339">Pixabay</a>
-    //
-    //
+    //Sound Effect by <a href="https://pixabay.com/users/dragon-studio-38165424/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=425455">DRAGON-STUDIO</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=425455">Pixabay</a>
+    //Sound Effect by <a href="https://pixabay.com/users/dragon-studio-38165424/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=386181">DRAGON-STUDIO</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=386181">Pixabay</a>
     //
     //
     //
